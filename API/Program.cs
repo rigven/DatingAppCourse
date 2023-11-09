854e72c81c5281ae3a5fa3cb97936f1de920896e
Yuliya Yakubenko <yuliya.yakubenko@mdcloud.tech>
2023-11-09 22:52:44 +0300

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,5 @@
-using System.Text;
-using API.Data;
-using API.Interfaces;
-using API.Services;
 using API.Extensions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +11,14 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
 //Configure the HTTP request pipeline
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
