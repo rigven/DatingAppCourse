3a6547c68b43d029ad3625192b907051de5a9468
Yuliya Yakubenko <yuliya.yakubenko@mdcloud.tech>
2023-11-09 13:56:04 +0300

    End of section 5

---
+++ API/Program.cs
@@ -0,0 +1,28 @@
+using System.Text;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+var app = builder.Build();
+
+//Configure the HTTP request pipeline
+app.UseCors(policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
